f05e82eea892c83fa9864dd5d6a0aec37f49782c
Zachary Stufflebeme <Stufflebeme.Zachary@gmail.com>
2022-09-07 14:32:47 -0500

    Here I demonstrate ability to search through tables in SQL

---
+++ tables_exercises.sql
@@ -0,0 +1,38 @@
+use employees;
+show tables;
+# tables in employees include: 
+#departments
+#dept_emp
+#dept_manager
+#employees
+#salaries
+#titles
+
+# Which table(s) contain a numeric type column?
+# dept_emp, dept_manager, employees, salaries, titles
+
+
+#Which table(s) contain a string type column?
+# Departments, dept_emp, dept_manager, employees, titles
+
+
+#which table(s) contain a date type column?
+# dept_emp, dept_manager, employees, salaries, titles
+
+
+#What is the relationship beetween the employees and departments tables?
+# There is no relationship between employees and departments table
+
+
+#Show the SQL that created the dept_manager table
+select * from dept_manager
+
+
+
+
+
+
+
+
+
+
